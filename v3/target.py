8e4fdbc27009fdf7ac0ca244c975eed06e1ed692
Ayan Chakrabarti <ayanc@ttic.edu>
2017-10-23 09:56:15 -0500

    v3

---
+++ v3/target.py
@@ -0,0 +1,116 @@
+#-- Ayan Chakrabarti <ayan@wustl.edu>
+
+
+import numpy as np
+import tensorflow as tf
+
+
+
+# tensorflow version compatibility
+def _unpack(x,num=None):
+    try:
+        return tf.unstack(x,num)
+    except:
+        return tf.unpack(x,num)
+    
+
+class Target:
+    def __init__(self,imin=0,imax=1.0,omin=0.25,omax=0.75,nBits=8):
+
+        self.imin = imin
+        self.imax = imax
+        self.omin = omin
+        self.omax = omax
+        self.nBits = nBits
+
+    def rSamp(self,bsz=64):
+        dl = np.random.uniform(1.,2.)*2.0**(-self.nBits) 
+        dl = np.float32(np.reshape([-dl,dl],[2,1]))*0.49*(self.omax-self.omin)
+        
+        sig = np.float32(self.imin + np.random.sample([1,bsz//2])*(self.imax-self.imin-dl[0,-1]))
+        sig = np.maximum(self.imin,np.minimum(self.imax,np.reshape(sig+dl,[bsz,1])))
+        ovec = self.s2o(sig)
+
+        return sig,ovec
+
+
+    def uSamp(self,bsz=8192):
+        sig = np.linspace(self.imin,self.imax,bsz+1)[:-1]
+        sig = np.float32(np.reshape(sig,[bsz,1]))
+        ovec = self.s2o(sig)
+
+        return sig,ovec
+
+    # Ground truth quantization function
+    def s2o(self,sig):
+        out = np.maximum(0.,sig-self.omin)
+        out = np.minimum(1.,out/(self.omax-self.omin))
+        out = np.int64(out*(2**self.nBits-1))
+        
+        ovec = []
+        for i in range((self.nBits+1)//8):
+            ovec = ovec + [np.unpackbits(np.uint8(out%256),axis=1)[:,::-1]]
+            out = out // 256
+        if len(ovec) == 1:
+            ovec = ovec[0]
+        else:
+            ovec = np.stack(ovec,axis=1)
+        ovec = ovec[:,:self.nBits]    
+        return ovec
+        
+
+
+    # Computes encoded value, error, and loss nodes
+    # Call with tensors / placeholders
+    def Eval(self,gt,hpred,actl=None):
+        wt = np.float32(2**np.arange(self.nBits))
+        wt = tf.constant(np.reshape(wt,[1,self.nBits]))
+
+        # Encoded value from predicted bits
+        hpred = tf.stop_gradient(hpred) # just in case
+        enc = tf.reduce_sum(wt*hpred,1,keep_dims=True)
+        egt = tf.reduce_sum(wt*gt,1,keep_dims=True)
+
+        # Compute soft-loss
+        loss = None
+        if actl is not None:
+            enc0 = enc-wt*hpred
+            enc1 = enc0+wt
+
+            # Direct Loss
+            wtpn = tf.abs(enc1-egt)-tf.abs(enc0-egt)
+            wtp = tf.square(tf.nn.relu(-wtpn))
+            wtn = tf.square(tf.nn.relu(wtpn))
+            loss0 = tf.reduce_mean(wtp*actl[0] + wtn*actl[1])
+
+            # Derivative Loss
+            egt2 = _unpack(tf.reshape(egt,[2,-1,1]))
+            hpd2 = _unpack(tf.reshape(enc,[2,-1,1]))
+            en02 = _unpack(tf.reshape(enc0,[2,-1,self.nBits]))
+            en12 = _unpack(tf.reshape(enc1,[2,-1,self.nBits]))
+
+            at02 = _unpack(tf.reshape(actl[0],[2,-1,self.nBits]))
+            at12 = _unpack(tf.reshape(actl[1],[2,-1,self.nBits]))
+
+
+            gdiff = egt2[0]-egt2[1]
+
+            wtpn = tf.abs(en12[0]-hpd2[1] - gdiff) - tf.abs(en02[0]-hpd2[1] - gdiff)
+            wtp = tf.square(tf.nn.relu(-wtpn))
+            wtn = tf.square(tf.nn.relu(wtpn))
+            loss1a = tf.reduce_mean(wtp*at02[0] + wtn*at12[0])
+
+            wtpn = tf.abs(hpd2[0]-en12[1] - gdiff) - tf.abs(hpd2[0] - en02[1] - gdiff)
+            wtp = tf.square(tf.nn.relu(-wtpn))
+            wtn = tf.square(tf.nn.relu(wtpn))
+            loss1b = tf.reduce_mean(wtp*at02[1] + wtn*at12[1])
+
+            ###
+            loss = loss0+0.5*(loss1a+loss1b)
+            
+        # Error in encoded numerical value
+        err1 = tf.reduce_mean(tf.abs(egt-enc))
+        err2 = tf.sqrt(tf.reduce_mean(tf.square(egt-enc)))
+
+        return [enc,loss,err1,err2]
+            
---
+++ v3/train.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+#-- Ayan Chakrabarti <ayan@wustl.edu>
+
+import sys
+import os
+import time
+import tensorflow as tf
+import numpy as np
+import ctrlc
+
+import utils as ut
+import circuit as ct
+import target as tg
+
+
+#########################################################################
+# Model save & logging setup
+ockp = ut.ckpter('wts/iter_*.state.npz') # Optimization state
+ockp_s = 'wts/iter_%d.state.npz'
+
+mckp = ut.ckpter('wts/iter_*.model.npz') # Model Files
+mckp_s = 'wts/iter_%d.model.npz'
+
+log = open('wts/train.log','a')
+
+
+def mprint(s):
+    sys.stdout.write(time.strftime("%Y-%m-%d %H:%M:%S ") + s + "\n")
+    log.write(time.strftime("%Y-%m-%d %H:%M:%S ") + s + "\n")
+    sys.stdout.flush()
+    log.flush()
+    
+
+    
+#########################################################################
+# Training Parameters
+
+maxiter=2e4
+lr=1e-2
+
+# Phase 2
+#maxiter=3e4
+#lr = 3.16e-3 # / Sqrt (10)
+
+# Phase 3
+#maxiter = 4e4
+#lr = 1e-3     # / 10
+
+
+mom = 0.9
+
+bsz = 4096
+bgroup = 256
+
+nBits = 8
+nHidden = 24
+
+sfreq = 1e3      # How frequently to save
+
+
+#########################################################################
+
+# Actual model
+data = tg.Target(nBits=nBits)
+ckt = ct.Circuit(nHidden=nHidden,nBits=nBits)
+
+gt = tf.placeholder(dtype=tf.float32)
+sig = tf.placeholder(dtype=tf.float32)
+hpred, acl = ckt.encode(sig)
+
+_,loss,err1,err2 = data.Eval(gt,hpred,acl)
+
+opt = tf.train.AdamOptimizer(lr,mom)
+tStep = opt.minimize(loss+1e-3*tf.reduce_sum(tf.abs(ckt.wts['w1'])))
+
+
+#########################################################################
+# Start TF session (respecting OMP_NUM_THREADS)
+# Restore model if necessary
+
+gopt = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gopt))
+try:
+    sess.run(tf.global_variables_initializer())
+except:
+    sess.run(tf.initialize_all_variables())
+
+if ockp.latest is not None:
+    mprint("Loading model")
+    ut.netload(ckt,'wts/iter_%d.model.npz'%ockp.iter,sess)
+    mprint("Loading state")
+    ut.loadopt(opt,ckt.wts,[],'wts/iter_%d.state.npz'%ockp.iter,sess)
+    mprint("Done!")
+
+niter = ockp.iter
+
+
+#########################################################################
+# Main training loop
+
+mprint("Starting from Iteration %d" % niter)
+while niter < maxiter:
+
+    e1v_a, e2v_a, lv_a = 0., 0., 0.
+    for i in range(bgroup):
+        sigs,bits = data.rSamp(bsz)
+        e1v,e2v,lv,_ = sess.run([err1,err2,loss,tStep],feed_dict={gt: bits, sig: sigs})
+        e1v_a = e1v_a+e1v
+        e2v_a = e2v_a+e2v
+        lv_a = lv_a+lv
+    e1v_a = e1v_a / bgroup
+    e2v_a = e2v_a / bgroup
+    lv_a = lv_a / bgroup
+    mprint("Iteration %09d [lr=%.2e]: Err1 = %.4e, Err2 = %.4e, Loss = %.4e" % (niter, lr, e1v_a, e2v_a, lv_a))
+    niter=niter+1
+        
+    if ctrlc.stop:
+        break
+
+    if sfreq > 0 and niter % sfreq == 0:
+        ut.netsave(ckt,mckp_s%niter,sess)
+        ut.saveopt(opt,ckt.wts,[],ockp_s%niter,sess)
+        ockp.clean(every=sfreq)
+        mckp.clean(every=sfreq)
+        mprint("Saved model and state.")
+
+
+mprint("Done!")
+ut.netsave(ckt,mckp_s%niter,sess)
+ut.saveopt(opt,ckt.wts,[],ockp_s%niter,sess)
+ockp.clean(every=sfreq)
+mckp.clean(every=sfreq)
+mprint("Saved model and state.")
+log.close()
